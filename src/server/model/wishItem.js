ef721e947ae97d12cbe55e9a51ee8254673c60e5
Artiom Oganesyan <artiomoganesyan@gmal.com>
2019-07-19 11:54:09 +0300

    server models done

---
+++ src/server/model/wishItem.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const wishItemSchema = new mongoose.Schema({
+  title: String,
+  img: String,
+  rating: String,
+  price: String,
+  description: String,
+  messenger: [{ userId: String, text: String }],
+  active: Boolean,
+  reserve: Boolean
+});
+
+module.exports = mongoose.model('WhisItem', wishItemSchema);
