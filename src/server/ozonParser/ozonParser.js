47abcde37380bc95f6159a97ecd85d67ed493c70
Filipp Loss <filipp.loss@gmail.com>
2019-07-23 11:47:48 +0300

    OZON parser functionality added w large pic, w/o integration to the  interface

--- src/server/ozonParser/ozonParser.js
+++ src/server/ozonParser/ozonParser.js
@@ -3,13 +3,14 @@ const puppeteer = require('puppeteer');
 const scrape = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.ozon.ru/context/detail/id/149997929/');
+  await page.goto('https://www.ozon.ru/context/detail/id/136937722/');
   await page.waitFor(1000);
   // Код для скрапинга
   const result = await page.evaluate(() => {
     const title = document.querySelector('h1').innerText;
     const price = document.querySelector("meta[itemprop='price']").content;
-    const pictureUrl = document.querySelector('._615bd').src;
+    const pictureUrl = document.querySelector("meta[itemprop='image']").content; // Large picture
+    // Small pic here: const pictureUrl = document.querySelector('._615bd').src;
     return {
       title,
       price,
