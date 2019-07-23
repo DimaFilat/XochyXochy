35f515f3ccaf984a6a1f79d4b1a91795c9f95155
Filipp Loss <filipp.loss@gmail.com>
2019-07-23 11:32:19 +0300

    OZON parser functionality added w/o integration to interface

---
+++ src/server/ozonParser/ozonParser.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+const scrape = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.ozon.ru/context/detail/id/149997929/');
+  await page.waitFor(1000);
+  // Код для скрапинга
+  const result = await page.evaluate(() => {
+    const title = document.querySelector('h1').innerText;
+    const price = document.querySelector("meta[itemprop='price']").content;
+    const pictureUrl = document.querySelector('._615bd').src;
+    return {
+      title,
+      price,
+      pictureUrl
+    };
+  });
+  browser.close();
+  return result;
+};
+
+scrape().then(value => {
+  console.log(value);
+});
